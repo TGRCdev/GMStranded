9a168a4d9f4fa2dd3be3b07153b0d45fd461d6e0
TGRCDev <tgrc@tgrc.dev>
2024-02-09 01:00:17 -0800

    Updated the nix dev shell
    I know how to use Nix now!

--- shell.nix
+++ shell.nix
@@ -1,12 +1,3 @@
-{ pkgs ? import <nixpkgs> {} }:
-let 
-    glualint = pkgs.fetchzip {
-        url = "https://github.com/FPtje/GLuaFixer/releases/download/1.26.0/glualint-1.26.0-x86_64-linux.zip";
-        sha256 = "sha256-ujT2uKduohMHb7TWJJTo9CYuikRNiTS3OGut4Zu+MZw=";
-    };
-in pkgs.mkShell {
-    name = "glualint-env";
-    shellHook = ''
-        PATH=${glualint}:$PATH
-    '';
-}
+(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+  src = builtins.fetchGit ./.;
+}).shellNix
